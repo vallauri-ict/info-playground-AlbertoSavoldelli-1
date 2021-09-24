39c8076df094726f5a9e344fa2ab239c2c750316
Stud. Alberto Savoldelli <a.savoldelli.1375@vallauri.net>
2021-09-24 10:49:01 +0200

    ese 02 terminated

---
+++ Es 2- Aereoporto/Es 2- Aereoporto/QueryCompito.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [dbo].[Aeroporto]
+(
+	[Città] NCHAR(20) NOT NULL PRIMARY KEY, 
+    [Nazione] NCHAR(20) NULL, 
+    [NumPiste] INT NULL
+)
+
+CREATE TABLE [dbo].[Volo]
+(
+	[IdVolo] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [GiornoSett] NCHAR(10) NULL, 
+    [CittaPart] NCHAR(20) NULL, 
+    [OraPart] TIME NULL, 
+    [CittaArr] NCHAR(20) NULL, 
+    [OraArr] TIME NULL, 
+    [TipoAereo] NCHAR(20) NULL
+)
+
+CREATE TABLE [dbo].[Aereo]
+(
+	[TipoAereo] NCHAR(20) NOT NULL PRIMARY KEY, 
+    [NumPasseggeri] INT NULL, 
+    [QtaMerci] INT NULL
+)
---
+++ Es 2- Aereoporto/Es 2- Aereoporto/QueryInClasse.sql
@@ -0,0 +1,14 @@
+﻿SELECT *
+FROM Volo v
+WHERE v.CittaArr='Roma'
+ORDER BY v.CittaPart
+
+SELECT a.Città
+FROM Aeroporto a
+WHERE a.NumPiste IS NULL
+
+SELECT v.IdVolo, a.NumPasseggeri
+FROM Aereo a, Volo v
+WHERE a.TipoAereo=v.TipoAereo 
+AND a.QtaMerci>0
+AND a.NumPasseggeri>0
