b5b30527cab400917409428fefa0354d892706fa
Stud. Alberto Savoldelli <a.savoldelli.1375@vallauri.net>
2021-10-01 11:03:15 +0200

    Es 4 some query done

---
+++ Es 4-SQL Musei/ES 4-SQL Musei/Query.sql
@@ -0,0 +1,15 @@
+﻿/*Per ciascun museo di Londra, il numero di opere di artisti italiani ivi conservate*/
+
+SELECT m.NomeM, count(*) as numeroOpere
+FROM Artisti a, Opere o, Musei m
+WHERE a.Nazionalita='IT'
+AND a.NomeA=o.NomeA
+AND m.NomeM=o.NomeM
+AND m.Citta='Londra'
+GROUP BY m.NomeM
+
+/*Il nome dei musei di londra che non hanno opere di Tiziano*/
+SELECT m.NomeM
+FROM Musei m
+WHERE  m.Citta='Londra'
+AND NOT EXISTS(SELECT * FROM Opere o WHERE o.NomeA='Tiziano' AND o.NomeM=m.NomeM)
