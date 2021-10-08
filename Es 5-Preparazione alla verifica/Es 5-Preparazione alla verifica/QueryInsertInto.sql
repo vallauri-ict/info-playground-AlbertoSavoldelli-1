5abeace9e1b4359f8552fbaf341406d1a8fb23fe
Stud. Alberto Savoldelli <a.savoldelli.1375@vallauri.net>
2021-10-08 12:05:49 +0200

    Es 5 continued

---
+++ Es 5-Preparazione alla verifica/Es 5-Preparazione alla verifica/QueryInsertInto.sql
@@ -0,0 +1,14 @@
+﻿INSERT INTO Proprietari(CodF,Nome,citta, stato)
+VALUES('ZKHXSV75P05G388C','Tomba','Pinerolo'),
+('PSYQCY74H21A218Z','Pape','Fossano'),
+('DSCKSB59A51C205T','Barbero','Mondovi')
+
+INSERT INTO Assicurazioni VALUES
+('Vittoria','Carignano'),
+('Toro','Pinerolo'),
+('Vittoria','Fossano')
+
+/*INSERT INTO Auto VALUES
+('FV448PO','Maserati','Ghibli',3000,330,'ZKHXSV75P05G388C',),
+('Toro','Pinerolo'),
+('Vittoria','Fossano')*/
