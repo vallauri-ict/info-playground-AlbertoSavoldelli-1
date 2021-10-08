9242acc02b09b217d8ebe1e2f2c131bff4f187c1
Stud. Alberto Savoldelli <a.savoldelli.1375@vallauri.net>
2021-10-08 11:20:45 +0200

    Es 5 continued

--- Es 5-Preparazione alla verifica/Es 5-Preparazione alla verifica/QueryCreateTable.sql
+++ Es 5-Preparazione alla verifica/Es 5-Preparazione alla verifica/QueryCreateTable.sql
@@ -1,17 +1,4 @@
-﻿CREATE TABLE [dbo].[Auto]
-(
-	[Targa] VARCHAR(7) NOT NULL PRIMARY KEY, 
-    [Marca] VARCHAR(30) NULL, 
-    [Modello] VARCHAR(30) NULL, 
-    [Cilindrata] INT NULL, 
-    [Potenza] INT NULL, 
-    [CodF] NCHAR(10) NULL, 
-    [CodAss] NCHAR(10) NULL, 
-    CONSTRAINT [FK_Auto_ToTable] FOREIGN KEY ([CodF]) REFERENCES [Proprietari]([CodF]), 
-    CONSTRAINT [FK_Auto_ToTable_1] FOREIGN KEY ([CodAss]) REFERENCES [Assicurazioni]([CodAss])
-)
-
-CREATE TABLE [dbo].[Proprietari]
+﻿CREATE TABLE [dbo].[Proprietari]
 (
 	[CodF] VARCHAR(20) NOT NULL PRIMARY KEY, 
     [Nome] VARCHAR(30) NULL, 
@@ -22,6 +9,18 @@ CREATE TABLE [dbo].[Assicurazioni]
 	[CodAss] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Nome] VARCHAR(30) NULL, 
     [Sede] VARCHAR(50) NULL
+)
+CREATE TABLE [dbo].[Auto]
+(
+	[Targa] VARCHAR(7) NOT NULL PRIMARY KEY, 
+    [Marca] VARCHAR(30) NULL, 
+    [Modello] VARCHAR(30) NULL, 
+    [Cilindrata] INT NULL, 
+    [Potenza] INT NULL, 
+    [CodF] VARCHAR(20) NULL, 
+    [CodAss] INT NULL, 
+    CONSTRAINT [FK_Auto_ToTable] FOREIGN KEY ([CodF]) REFERENCES [Proprietari]([CodF]), 
+    CONSTRAINT [FK_Auto_ToTable_1] FOREIGN KEY ([CodAss]) REFERENCES [Assicurazioni]([CodAss])
 )
 CREATE TABLE [dbo].[Sinistro]
 (
