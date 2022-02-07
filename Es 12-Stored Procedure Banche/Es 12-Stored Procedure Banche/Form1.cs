4f2bd3f9bda132557ded469ce10696609fb61bb3
AlbertoSavoldelli <a.savoldelli.1375@vallauri.edu>
2022-02-07 12:53:19 +0100

    Some esercises done

---
+++ Es 12-Stored Procedure Banche/Es 12-Stored Procedure Banche/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Es_12_Stored_Procedure_Banche
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
